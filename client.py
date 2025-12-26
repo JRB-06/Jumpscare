e3334eda39f0474c4c9af3a3841338cd05435249
Your Name <jrbinghoy2006@gmail.com>
2025-12-27 00:32:12 +0800

    inital features

---
+++ client.py
@@ -0,0 +1,33 @@
+import discord
+import logging
+import os 
+from main import Jumpscare
+from dotenv import load_dotenv
+load_dotenv()
+
+class Jumpscare_client(discord.Client):
+    
+    async def on_ready(self):
+        print(f'Bot is Active {self.user}!')
+        
+    async def on_message(self, message: discord.Message):
+        if message.author == self.user:
+            return
+        print(f'======================\nAuthor: {message.author} \nChannel: {message.channel} \nGuild: {message.guild}\n======================\n')
+        
+        j = Jumpscare(message)
+        
+        if message.content.startswith('$jumpscare'):
+            print("Force Jumpscare\n")
+            await j.jumpscare_msg()
+        else:
+            await j.jumpscare_trigger()
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Jumpscare_client (intents=intents)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+client.run(os.getenv('TOKEN'), log_handler=handler)
+
